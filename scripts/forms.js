946dedf20954b7be5f5e92c5083ac6fc8ed42cf9
PneuJai <53138480+Rjlintkh@users.noreply.github.com>
2020-12-23 17:13:38 +0800

    Added function "sendForm"

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,4 +1,5 @@
-const { ModalFormRequest } = require("bdsx/packetId");
+
+var { netevent, createPacket, sendPacket, PacketId } = require("bdsx");
 
 class SimpleForm {
     constructor(title = "", content = "") {
@@ -118,3 +119,27 @@ class CustomForm {
 }
 
 exports.CustomForm = CustomForm;
+
+const formHandlers = {};
+
+function sendForm(networkIdentifier, form, handler = () => {}) {
+    let formId = parseInt(new Date().getTime() / 1000);
+    let packet = createPacket(PacketId.ModalFormRequest);
+    packet.setUint32(formId, 0x28);
+    packet.setCxxString(JSON.stringify(form), 0x30);
+    sendPacket(networkIdentifier, packet);
+    packet.dispose();
+    formHandlers[formId] = handler;
+    return formId;
+}
+
+exports.sendForm = sendForm;
+
+netevent.raw(PacketId.ModalFormResponse).on((ptr, size, networkIdentifier) => {
+    let data = {};
+    ptr.move(1);
+    data.formId = ptr.readVarUint();
+    data.formData = ptr.readVarString();
+    formHandlers[data.formId](data, networkIdentifier);
+    delete formHandler[formId];
+});
