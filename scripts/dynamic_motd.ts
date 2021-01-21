eb924587c1b44a61d8de0f8631ed0c58ceaf0c92
PneuJai <samueltkho0331@gmail.com>
2021-01-21 19:18:19 +0800

    Added an example

--- scripts/dynamic_motd.ts
+++ scripts/dynamic_motd.ts
@@ -4,3 +4,9 @@ import { wrappedFunc } from "./exe_exports";
 export function setMotd(motd: string) {
     wrappedFunc["ServerNetworkHandler::allowIncomingConnections"](serverInstance.minecraft.something.shandler, motd, true);
 }
+
+/* example
+setInterval(() => {
+    setMotd(new Date().toLocaleTimeString())
+}, 5000);
+*/
